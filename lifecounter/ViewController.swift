1fe6f1bc4564cc769032f4e963957a1600330759
Chaitanya Gangavarapu <chaitanya.gangavarapu@gmail.com>
2019-01-28 15:38:30 -0800

    Initial Commit

---
+++ lifecounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by Chai Gangavarapu on 1/28/19.
+//  Copyright © 2019 Chaitanya Gangavarapu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
